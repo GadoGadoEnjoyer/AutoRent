6ec049ac2568cb5ccbc47c478e954448074b2226
Alireza Arifi <alirezaarifi69@gmail.com>
2023-08-02 10:35:24 +0700

    Bootstrap bull----

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ADMIN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
 </head>
 <body>
     {{-- @foreach ($users as $user)
@@ -47,5 +49,6 @@
         <br>
     @endforeach
     <button onclick="window.location.href='{{ route('bukabuatdiskon') }}'">Tambah Diskon</button>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/buatdiskon.blade.php
+++ resources/views/buatdiskon.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Buat Diskon!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
 </head>
 <body>
     <form action="{{ route('buatdiskon') }}" method="POST">
@@ -12,5 +14,6 @@
         <input type="text" name="nama_diskon" placeholder="Nama Diskon">
         <input type="number" name="jumlah_diskon" placeholder="Jumlah Diskon">
         <button type="submit">Buat Diskon</button>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
