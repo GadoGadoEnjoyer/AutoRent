b78b53932aa753c49ebc856332f9124f6ee94fa0
Alireza Arifi <alirezaarifi69@gmail.com>
2023-08-03 14:02:00 +0700

    Welcome Page Done!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,53 +6,45 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to AutoRent!</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@400;700&display=swap" rel="stylesheet">
   </head>
+  <style>
+    .butt{
+        background-color:rgba(66, 66, 66, 0.275);
+        color:white;
+    }
+    .butt:hover{
+        background-color:white;
+        color:black;
+    }
+  </style>
+<body class="h-100 m-0 p-0">
+    <div class="wallpaper" style="background-image: 
+    linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.6) 40%, rgba(0,0,0,0.15) 60%, rgba(0,0,0,0) 100%),
+    url('../image/wallpaper4.jpg'); background-size: cover; background-position: center; height: 100vh; width: 100vw; position: absolute; top: 0; left: 0; font-family: 'Lexend Deca', sans-serif; color:white !important;">
 
-</head>
-<body>
-    <div>
-        <div>
+        <div class="header" style="display:grid;grid-auto-flow: column;grid-template-columns:1fr 1fr 8fr 2fr;align-items:center;">
+            <a href='{{ route("bukaregister") }}' style="color:white !important;text-decoration:none !important;cursor:pointer;justify-self:end">Register</a>
             @if(Auth::user())
-            <div>
-                <button onclick="window.location='{{ route("logout") }}'"><img src="../image/door-check-out-icon.svg"></button>
-                <br>
-                <span>Log Out</span>
-            </div>
+            <a href='{{ route("logout") }}' style="color:white !important;text-decoration:none !important;cursor:pointer;justify-self:end">Logout</a>
             @else
-            <div>
-                <button onclick="window.location='{{ route("bukalogin") }}'"><img src="../image/door-check-in-icon.svg"></button>
-                <br>
-                <span>Log In</span>
-            </div>
+            <a href='{{ route("bukalogin") }}' style="color:white !important;text-decoration:none !important;cursor:pointer;justify-self:end">Login</a>
             @endif
-
-            <div>
-                <button onclick="window.location='{{ route("bukaregister") }}'"><img src="../image/user-plus-icon.svg"></button>
-                <br>
-                <span>Register</span>
-            </div>
-            <div>
-                <p>Welcome to AutoRent!</p>
-            </div>
+            <p class="title text-center" style="font-size : clamp(3rem, 7.6vw, 8rem);font-weight:700;text-shadow: 12px 4px 4px black;">AUTO RENT</p>
             @if(Auth::user())
-            <div>
-                <button onclick="window.location='{{ route("bukaprofile",Auth::User()) }}'"><img src="../image/business-card-icon.svg"></button>
-                <br>
-                <span>Profile</span>
-            </div>
-            @else
-            <div>
-            </div>
+            <a href='{{ route("bukaprofile",Auth::User()) }}' style="color:white !important;text-decoration:none !important;cursor:pointer;justify-self:start">Profile</a>
             @endif
         </div>
-        <br>
-        <div>
-            <div>
-                <button onclick="window.location='{{ route("registerCar") }}'"><img src="../image/access-hand-key-icon.svg"><br><span>Rent Your Car!</span></button>
-            </div>
-            <div>
-                <button onclick="window.location='{{ route("carlist") }}'"><img src="../image/rent-a-car-icon.svg"><br><span>Rent some stranger Car!</span></button>
-            </div>
+
+        <div style="display:flex;justify-content:space-evenly;font-size : clamp(2rem, 4vw, 4rem);margin-top:clamp(6rem,15vw,15rem);">
+            <button class="butt" onclick="window.location='{{ route("registerCar") }}'">
+                RENT YOUR CAR
+            </button>
+            <button class="butt" onclick="window.location='{{ route("carlist") }}'">
+               RENT SOMEONE CAR
+            </button>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
